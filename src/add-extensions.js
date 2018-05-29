74a5ab8752444fa2971fb173751f0f22923d7616
Florent BENOIT <fbenoit@redhat.com>
2018-05-29 13:30:26 +0200

    Handle as well 0.3.10 version

--- src/add-extensions.js
+++ src/add-extensions.js
@@ -75,6 +75,7 @@ function addExtensions(extensions) {
 function replaceToWipTheia(extensionRootPath) {
     cp.execSync(`find ${extensionRootPath} -type f -exec sed -i -e 's/@theia/@wiptheia/g' {} \\;`);
     cp.execSync(`find ${extensionRootPath} -type f -exec sed -i -e 's/0.3.7/latest/g' {} \\;`);
+    cp.execSync(`find ${extensionRootPath} -type f -exec sed -i -e 's/0.3.10/latest/g' {} \\;`);
     cp.execSync(`find ${extensionRootPath} -type f -name "yarn.lock" -exec rm {} \\;`);
 }
 
